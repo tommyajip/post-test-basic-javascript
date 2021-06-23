fda319712876e2863cde6925016835f033975dfe
Tommy Aji Prabowo <44915043+tommyajip@users.noreply.github.com>
2021-06-23 16:25:51 +0700

    post test - basic javascript - nomor 3c

--- 3c/script.js
+++ 3c/script.js
@@ -8,6 +8,6 @@ const user = {
     universitas: "Universitas Indonesia",
     alamat: "Drojogan RT 05 RW 03, Bumirejo, Mungkid, Magelang",
     email: "tommy.aji@ui.ac.id"
-};
+}; 
 
 console.log(`Perkenalkan nama saya ${user.nama}, saya lahir di ${user.tempatLahir}, pada tanggal ${user.tanggalLahir}. Saat ini saya berumur ${user.umur} tahun. Saya kuliah di ${user.jurusan}, ${user.fakultas}, ${user.universitas}. Alamat saya saat ini di ${user.alamat}. Teman-teman dapat menghubungi saya melalui email saya di ${user.email}. Salam kenal dan terima kasih.`);
