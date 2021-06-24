b8a8382af419c846bf5821b8cf9d05e9729834cc
Tommy Aji Prabowo <44915043+tommyajip@users.noreply.github.com>
2021-06-24 18:53:56 +0700

    post test - basic javascript - nomor 3b

--- 3b/script.js
+++ 3b/script.js
@@ -3,7 +3,7 @@ let lagi = true;
 while (lagi === true) {
     const phi = 3.14;
     let r = prompt("Masukkan ukuran jari-jari: ");
-    let hasil = phi * Number(r) * r;
+    let hasil = phi * Number(r) * Number(r);
     alert(`Luas lingkaran dengan jari-jari = ${r} adalah ${hasil}`);
     lagi = confirm("Apakah Anda ingin menghitung luas lingkaran lagi?");
 }
