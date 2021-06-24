70d18a6486585c95154a6e4f717d581af45a0bd8
Tommy Aji Prabowo <44915043+tommyajip@users.noreply.github.com>
2021-06-24 18:50:08 +0700

    post test - basic javascript - nomor 3a

--- 3a/script.js
+++ 3a/script.js
@@ -3,7 +3,7 @@ let lagi = true;
 while (lagi === true) {
     let a = prompt("Masukkan ukuran alas: ");
     let t = prompt("Masukkan ukuran tinggi: ");
-    let hasil = 0.5 * a * t;
+    let hasil = 0.5 * Number(a) * Number(t);
     alert(`Luas segitiga dengan alas = ${a} dan tinggi = ${t} adalah ${hasil}`);
     lagi = confirm("Apakah Anda ingin menghitung luas segitiga lagi?");
 }
